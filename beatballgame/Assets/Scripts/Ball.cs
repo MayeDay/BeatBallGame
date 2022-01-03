dc644360c483b887f542ce44b096db96af188eb7
James Allen Maye III (Akarian) <jamesmaye6094@gmail.com>
2022-01-04 02:17:36 +0800

    progress update

--- beatballgame/Assets/Scripts/Ball.cs
+++ beatballgame/Assets/Scripts/Ball.cs
@@ -5,14 +5,21 @@ using UnityEngine;
 public class Ball : MonoBehaviour
 {
     //Define private variables
-    float _speed = 24f;
+    float _speed = 20f;
     Rigidbody _rigidbody;
     Vector3 _velocity;
+    Renderer _renderer;
 
     void Start()
     {
         _rigidbody = GetComponent<Rigidbody>();
-        _rigidbody.velocity = Vector3.down * _speed;
+        _renderer = GetComponent<Renderer>();
+        Invoke("Launch", 0.5f);
+    }
+
+    void Launch() 
+    {
+        _rigidbody.velocity = Vector3.up * _speed;
     }
 
     void FixedUpdate()
@@ -20,6 +27,12 @@ public class Ball : MonoBehaviour
         //the ball will have 20 units per second speed
         _rigidbody.velocity = _rigidbody.velocity.normalized * _speed;
         _velocity = _rigidbody.velocity;
+
+        if (!_renderer.isVisible)
+        {
+            GameManager.Instance.Balls--;
+            Destroy(gameObject);
+        }
     }
 
     private void OnCollisionEnter(Collision collision) {
