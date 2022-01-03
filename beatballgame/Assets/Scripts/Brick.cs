a5320552df19427daa6a87c7f9a9f29f952939be
James Allen Maye III (Akarian) <jamesmaye6094@gmail.com>
2022-01-04 02:21:34 +0800

    updated progress

--- beatballgame/Assets/Scripts/Brick.cs
+++ beatballgame/Assets/Scripts/Brick.cs
@@ -6,16 +6,23 @@ public class Brick : MonoBehaviour
 {
     public int hits = 1;
     public int points = 10;
+    public Vector3 rotator;
+    public Material hitMaterial;
+
+    Material _orgMaterial;
+    Renderer _renderer;
 
     void Start()
     {
-        
+        //transform.Rotate(rotator * (transform.position.x + transform.position.y)* 0.1f);
+        _renderer = GetComponent<Renderer>();
+        _orgMaterial = _renderer.sharedMaterial;
     }
 
     
     void Update()
     {
-        
+        //transform.Rotate(rotator * Time.deltaTime);
     }
 
     void OnCollisionEnter(Collision collision)
@@ -26,5 +33,11 @@ public class Brick : MonoBehaviour
             //Destroy() is a predefined variable that you can access to this game object to destroy the object
             Destroy(gameObject);
         }
+        _renderer.sharedMaterial = hitMaterial;
+        Invoke("RestoreMaterial", 0.05f);
+    }
+    void RestoreMaterial()
+    {
+        _renderer.sharedMaterial = _orgMaterial;
     }
 }
